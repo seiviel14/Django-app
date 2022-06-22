796c64e1b107767379c4cf093f8bc5bd2c4d8d48
seiviel14 <senriquevelasquez@gmail.com>
2022-06-22 17:03:13 +0200

    Security error

--- PremiosPlatziApp/settings.py
+++ PremiosPlatziApp/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1(sim(gsa7cr!l0%f0p#8x056q+ykhfu6=8ob6tti)%w!b^z9n'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
